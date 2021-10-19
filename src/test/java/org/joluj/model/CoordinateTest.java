b41b1db84ec0f09e7be3dcfc78262279c1f94865
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-10-19 15:47:09 +0200

    add coordinate tests

---
+++ src/test/java/org/joluj/model/CoordinateTest.java
@@ -0,0 +1,25 @@
+package org.joluj.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CoordinateTest {
+
+  static double DELTA = 0.000001;
+
+  @Test
+  public void testSuccessfulCreationFromString() {
+    var expected = new Coordinate(1, 2.3, 4.5);
+    var actual = new Coordinate("cartesian|1|2.3|4.5");
+    assertEquals(expected.getX(), actual.getX(), DELTA);
+    assertEquals(expected.getY(), actual.getY(), DELTA);
+    assertEquals(expected.getZ(), actual.getZ(), DELTA);
+  }
+
+  @Test
+  public void toStringMethod() {
+    var actual = new Coordinate("cartesian|1|2.3|4.5");
+    assertEquals("cartesian|1.0|2.3|4.5", actual.toString());
+  }
+}
