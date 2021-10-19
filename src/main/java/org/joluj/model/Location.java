bcc624dcaff54fe846b04e5d757295a3e5dde48f
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-10-19 15:59:33 +0200

    add coordinate validation

--- src/main/java/org/joluj/model/Location.java
+++ src/main/java/org/joluj/model/Location.java
@@ -3,10 +3,20 @@ package org.joluj.model;
 public class Location {
   public Coordinate coordinate;
 
+  /**
+   * @throws IllegalArgumentException if coordinate is null
+   */
   public Location(Coordinate coordinate) {
+    if (coordinate == null) {
+      throw new IllegalArgumentException("Coordinate must not be null");
+    }
     this.coordinate = coordinate;
   }
 
+  /**
+   * Creates a Location object from a string.
+   * The string must follow {@link Coordinate#toString() this scheme}.
+   */
   public Location(String location) {
     this.coordinate = new Coordinate(location);
   }
