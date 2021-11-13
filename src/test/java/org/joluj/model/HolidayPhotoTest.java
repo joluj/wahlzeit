c2e02b9c43d34a2c77a030a8f290e8b4b3765cf5
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-11-13 20:46:32 +0100

    update tests

--- src/test/java/org/joluj/model/HolidayPhotoTest.java
+++ src/test/java/org/joluj/model/HolidayPhotoTest.java
@@ -2,12 +2,17 @@ package org.joluj.model;
 
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.wahlzeit.model.PhotoStatus;
+import org.wahlzeit.model.Tags;
+import org.wahlzeit.services.EmailAddress;
+import org.wahlzeit.services.Language;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class HolidayPhotoTest {
@@ -31,7 +36,27 @@ public class HolidayPhotoTest {
     var photo = new HolidayPhoto(resultSet);
 
     assertFalse(photo.isDirty());
-    Mockito.verify(resultSet, Mockito.times(9));
+    assertEquals(photo.getCountry(), "China");
+  }
+
+  @Test
+  public void writeTo() throws SQLException, MalformedURLException {
+    // prepare
+    ResultSet resultSet = Mockito.mock(ResultSet.class);
+    var photo = new HolidayPhoto();
+    photo.setCountry("USA");
+    // other fields without null checks in {@link Photo#writeOn}
+    photo.setOwnerEmailAddress(EmailAddress.EMPTY);
+    photo.setOwnerHomePage(new URL("https://example.com"));
+    photo.setOwnerLanguage(Language.JAPANESE);
+    photo.setTags(Tags.EMPTY_TAGS);
+    photo.setStatus(PhotoStatus.DELETED);
+
+    // real act
+    photo.writeOn(resultSet);
+
+    // assert
+    Mockito.verify(resultSet, Mockito.times(1)).updateString("country", "USA");
   }
 
 }
